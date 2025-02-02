058f80d36cd2410d23845dedef39574d7c99a169
Khennee <iambishawn@gmail.com>
2025-02-02 18:14:51 +0800

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -11,32 +11,32 @@ const AboutPage = () => {
 
     const teamMembers = [
         { 
-            name: "Lorem Ipsum", 
+            name: "Alair, Shawn Khennee", 
             title: "CEO", 
             role: "Full-stack Developer" 
         },
         { 
-            name: "Lorem Ipsum", 
+            name: "Dador. Wendell Terrence", 
             title: "COO", 
             role: "Full-stack Developer" 
         },
         { 
-            name: "Lorem Ipsum", 
+            name: "Samenian, Joshua", 
             title: "CTO", 
             role: "Full-stack Developer - Designer" 
         },
         { 
-            name: "Lorem Ipsum", 
+            name: "Gerona, John Patrick", 
             title: "CIO", 
             role: "Full-stack Developer" 
         },
         { 
-            name: "Lorem Ipsum", 
+            name: "Genito, Matt Rafael", 
             title: "CMO", 
             role: "Marketing" 
         },
         { 
-            name: "Lorem Ipsum", 
+            name: "Poral, Danielle Anne", 
             title: "CAO", 
             role: "Front-end Developer - Designer" 
         },
@@ -66,25 +66,22 @@ const AboutPage = () => {
                 {teamMembers.map((member, index) => (
                     <div 
                     key={index} 
-                    className="mt-4 text-center"
+                    className="mt-4 text-center border-b-2 rounded-lg py-2 px-4 bg-gray-1000"
                     >
-                        <h1 className="text-xl font-semibold">{member.name}</h1>
+                        <h1 className="text-xl">{member.name}</h1>
                         <h2 className="text-lg text-titlecolor">{member.title}</h2>
                         <h3 className="text-md">{member.role}</h3>
                     </div>
                 ))}
             </div>
 
-            <div className="mt-12 text-center">
+            <div className="mt-8 text-center">
                 <h1 className="text-xl text-titlecolor"
                 >
                     Contact Us!
                 </h1>
-                <div className="mt-4 text-left px-6">
-                    <h2>Instagram: loremipsum@gmail.com</h2>
-                    <h2>Facebook: loremipsum@gmail.com</h2>
-                    <h2>Youtube: loremipsum@gmail.com</h2>
-                    <h2>LinkedIn: loremipsum@gmail.com</h2>
+                <div className="mt-2 text-left px-6">
+                    <h2>Email: playvour2024@gmail.com</h2>
                 </div>
             </div>
 
